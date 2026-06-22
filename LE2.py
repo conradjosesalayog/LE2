f88b5da9380e26aff81483dec851d56eb591f65d
conradjosesalayog <radradsalayog@gmail.com>
2026-06-22 20:35:14 +0800

    Draft for part 1. 3rd question not yet done

---
+++ LE2.py
@@ -0,0 +1,23 @@
+import pandas as pd
+df = pd.read_csv("sleep_health_data.csv")
+
+print(df.columns)
+print()
+
+# Question 1
+lowest_sleep_occ = df.groupby("Occupation")["Sleep Duration"].mean().idxmin()
+print("Occupation with the lowest average sleep duration:")
+print(lowest_sleep_occ)
+print()
+
+# Question 2
+lowest_sleep_quality_occ = df.groupby("Occupation")["Quality of Sleep"].mean().idxmin()
+print("Occupation with the lowest average sleep quality:")
+print(lowest_sleep_quality_occ)
+print()
+
+# Question 3
+same_occ_variable = lowest_sleep_occ == lowest_sleep_quality_occ
+print("Is it the same occupation?")
+print(same_occ_variable)
+print()
